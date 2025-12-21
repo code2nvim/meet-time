f361a512439289792c2be7cbf53b0ce515110ad1
code2nvim <code2nvim@gmail.com>
2025-12-22 00:04:39 +0800

    add default value for plan time

--- src/main/front/src/components/Plan/Create.tsx
+++ src/main/front/src/components/Plan/Create.tsx
@@ -1,6 +1,7 @@
 import { Form } from "@inertiajs/react";
 
 export function Create() {
+  const day = location.pathname.split("/").slice(2).join("-");
   return (
     <div>
       <h1 className="text-center text-xl">Create a plan</h1>
@@ -10,19 +11,32 @@ export function Create() {
         className="flex flex-col items-start gap-1.5 p-4"
       >
         <label className="text-right">Title:</label>
-        <input type="text" name="title" className="border bg-teal-300 p-0.5" />
+        <input
+          type="text"
+          name="title"
+          required
+          className="border bg-teal-300 p-0.5"
+        />
         <label className="text-right">Place:</label>
-        <input type="text" name="meetAt" className="border bg-teal-300 p-0.5" />
+        <input
+          type="text"
+          name="meetAt"
+          required
+          className="border bg-teal-300 p-0.5"
+        />
         <label className="text-right">Time:</label>
         <input
           type="text"
           name="meetTime"
+          required
+          defaultValue={`${day}T00:00:00`}
           className="border bg-teal-300 p-0.5"
         />
         <label className="text-right">Description:</label>
         <input
           type="text"
           name="description"
+          required
           className="h-48 w-full border bg-teal-300 p-0.5"
         />
         <button type="submit" className="mx-auto border bg-teal-300 p-0.5">
